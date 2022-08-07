7eef764eac22f849367f40d3f701a940e3c50721
Azt1 <inho.ilha@hotmail.com>
2022-08-07 14:17:42 -0300

    Primeiro componente

---
+++ fundamentos-app/src/components/basicos/Primeiro.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Primeiro() {
+    const msg = 'Seja Bem-Vindo'
+    return (
+        <div>
+            <h2>Primeiro Componete</h2>
+            <p>
+                {msg}
+            </p>
+        </div>
+    )
+}
