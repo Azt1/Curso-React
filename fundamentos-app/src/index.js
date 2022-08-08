8b2e989f85bca41b18a930a7db51a1ff7a636776
Azt1 <inho.ilha@hotmail.com>
2022-08-07 22:51:04 -0300

    Componentes

--- fundamentos-app/src/index.js
+++ fundamentos-app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-  <div>
+  <div id='app'>
     <Primeiro>ss</Primeiro>
+    <ComParametro
+      titulo = 'Situaçao do aluno'
+      aluno = 'Igor' nota={9.3}/>
   </div>,
   document.getElementById('root')
 )
